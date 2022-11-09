bc39c98de28cfeb62835318b8b965e562d615e9f
Pragat Pandya <pragat.pandya@gmail.com>
2022-11-09 19:43:46 +0530

    Revert "Add Allowed Hosts"
    This reverts commit 0406d8c79dcb1579f25c40f9a32c71fdaaf75bea.

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = ['django-ecom.eba-vn9ir44u.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
